9c406e863ed81c4e5969b5f45d5607096605dea0
Ayaanyx7 <ayaanyx7@gmail.com>
2026-06-20 19:17:38 +0100

    Delete cmake/Platforms/Android.cmake

--- cmake/Platforms/Android.cmake
+++
@@ -1,9 +0,0 @@
-# Android Platform Toolchain Bindings for Hatch Engine
-set(PLATFORM_LIBS log android GLESv2 EGL)
-add_definitions(-D__ANDROID__)
-add_definitions(-DDEFAULT_TARGET_NAME="HatchEngine")
-
-# Search and bind SDL2 Android specific implementation layers
-find_package(SDL2 REQUIRED)
-include_directories(${SDL2_INCLUDE_DIRS})
-list(APPEND PLATFORM_LIBS ${SDL2_LIBRARIES})
